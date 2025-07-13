560d77989be58cf30abacdc9b200b6194d9ffb5f
LUNAxKRISHNA <krishnaolassa@gmail.com>
2025-07-13 11:07:31 +0530

    P_1

---
+++ Problem_1/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.print("Enter target: ");
+        int target = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    System.out.println("Indices: [" + i + ", " + j + "]");
+                    return;
+                }
+            }
+        }
+    }
+}
