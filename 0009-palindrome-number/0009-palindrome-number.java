0418181b6dbb2d6d4c112a5edbf350250ed12f9f
Krishna <126711342+LUNAxKRISHNA@users.noreply.github.com>
2026-05-02 20:55:42 +0530

    0009-palindrome-number .java Easy

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int rem=0;
+        int num=x;
+        while(x>0){
+            rem=x%10;
+            rev=(rev*10)+rem;
+            x/=10;
+        }
+        if(num==rev) return true;
+        else{ return false;}
+        }
+    }
