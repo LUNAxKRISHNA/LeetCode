dec1910ce1c3fa86f99a18a935489705d97c06cd
Krishna <126711342+LUNAxKRISHNA@users.noreply.github.com>
2026-08-11 14:29:18 +0530

    0242-valid-anagram .java Easy

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] arr = new int[26];
+        for(char x: s.toCharArray()) arr[x-'a'] ++;
+        for(char x: t.toCharArray()) arr[x-'a'] --;
+        for(int x:arr) if(x!=0) return false;
+        return true;
+    }
+}
