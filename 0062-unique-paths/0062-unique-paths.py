833b8c9f6396a15f33303875563ed625fd3d9e61
Krishna <126711342+LUNAxKRISHNA@users.noreply.github.com>
2026-08-12 11:40:41 +0530

    0062-unique-paths .py Medium

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,4 +1,10 @@
-import math
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m+n-2,m-1)
+        dp=[[0]*n for _ in range(m)]
+        def solve(i,j):
+            if i==0 or j==0:
+                return 1
+            elif dp[i][j]==0:
+                dp[i][j]=solve(i-1,j) + solve(i,j-1)
+            return dp[i][j]
+        return solve(m-1,n-1)   
